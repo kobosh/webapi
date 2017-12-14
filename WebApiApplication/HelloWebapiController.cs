1e83119ebb5b8d1bbaa2290f9fab50ac69a6afac
kobosh <kokobosh@gmail.com>
2017-12-13 20:45:20 -0600

    6th commit

--- WebApiApplication/HelloWebapiController.cs
+++ WebApiApplication/HelloWebapiController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+
+
+
 namespace WebApiApplication
 {
     public class Student
@@ -10,17 +12,14 @@ namespace WebApiApplication
         public int StudentID { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
     }
-    // [EnableCors(origins: "*", headers: "*", methods: "*")]
-    public class StudensController : ApiController
+
+    public class StudentsController : ApiController
     {
 
         public HttpResponseMessage Get()
         {
-            HttpRequestMessage Request = new HttpRequestMessage();
-            HttpResponseMessage response =
-                Request.CreateResponse(HttpStatusCode.OK,
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK,
                 StudentRepository.GetAllStudents());
             response.Headers.Add("Access-Control-Allow-Origin", "*");
             return response;
@@ -36,69 +35,18 @@ namespace WebApiApplication
           new Student(){ StudentID=3, FirstName="Hussan",LastName="Mohu"},
            new Student(){ StudentID=4, FirstName="Hessan",LastName="Moham"}
     };
-      /*  HttpResponseMessage GetStudents()
-        {
-            var req = new HttpClient();
-            HttpResponseMessage response =
-            Request.CreateResponse(HttpStatusCode.OK, StudentRepository.GetAllStudents());
-            response.Headers.Add("Access-Control-Allow-Origin", "*");
 
-            return response;
-        }*/
 
-        public static List<Student> GetAllStudents()
+
+
+        static public List<Student> GetAllStudents()
         {
             return students;
         }
 
-        public static Student GetStudent(int studentID)
-        {
-            return students.Where(d => d.StudentID == studentID).First();
-        }
 
-        public static void RemoveStudent(int studentID)
-        {
-            var stu = students.Find(s => s.StudentID == studentID);
-            students.Remove(stu);
-        }
-
-        public static void AddStudent(Student student)
-        {
-            students.Add(student);
-        }
-
-        public static void UpdateStudent(Student student)
-        {
-            //
-        }
 
     }
-    public class StudentsController : ApiController
-    {
-        public List<Student> Get()
-        {
-            return StudentRepository.GetAllStudents();
-        }
 
-        public Student Get(int id)
-        {
-            return StudentRepository.GetStudent(id);
-        }
-
-        public void Post(Student Student)
-        {
-            StudentRepository.AddStudent(Student);
-        }
-
-        public void Put(Student Student)
-        {
-            StudentRepository.UpdateStudent(Student);
-        }
-
-        public void Delete(int id)
-        {
-            StudentRepository.RemoveStudent(id);
-        }
-    }
 
 }
